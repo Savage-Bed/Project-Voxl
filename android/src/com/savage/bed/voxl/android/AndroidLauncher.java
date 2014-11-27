b4135ef079220a92198f4904326fbcfdef169c49
SavageBedDev <savage.bed.official@gmail.com>
2014-11-27 17:05:24 -0500

    Initial Creation

---
+++ android/src/com/savage/bed/voxl/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.savage.bed.voxl.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.savage.bed.voxl.VoxlMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new VoxlMain(), config);
+	}
+}
---
+++ desktop/src/com/savage/bed/voxl/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.savage.bed.voxl.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.savage.bed.voxl.VoxlMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new VoxlMain(), config);
+	}
+}
